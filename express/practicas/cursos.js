32d02e307b444db0747ea51f042544c7cc9a1acf
Eliezer Solano Martinez <eliezersol50@gmail.com>
2023-01-14 18:14:08 -0600

    organice files

--- express/practicas/cursos.js
+++ express/practicas/cursos.js
@@ -1,16 +1,16 @@
-let infoCurso = {
+/* let infoCurso = {
     'programacion':[
         {
             id:1,
             titulo: 'Aprende Node',
-            lenguaje: 'Javascript',
+            lenguaje: 'javascript',
             vistas: 15000,
             nivel: 'Basico'
 
         },{
             id:2,
             titulo: 'Node Intermedio',
-            lenguaje: 'Javascript',
+            lenguaje: 'javascript',
             vistas: 18000,
             nivel: 'Intermedio'
         },{
@@ -26,18 +26,72 @@ let infoCurso = {
         {
             id:1,
             titulo: 'Aprende Calculo',
-            tema: 'Calculo',
+            tema: 'calculo',
             vistas: 28500,
             nivel: 'Basico'
         },
         {
             id:2,
             titulo: 'Aprende Fisica',
-            tema: 'Fisica',
+            tema: 'fisica',
             vistas: 28500,
             nivel: 'Basico'
         }
     ]
+} */
+
+
+let infoCurso= {
+    'cursos':{
+        'categorias':{
+        'matematicas':[
+            {
+                id:1,
+                titulo: 'Aprende Calculo',
+                tema: 'calculo',
+                vistas: 28500,
+                nivel: 'Basico'
+            },
+            {
+                id:2,
+                titulo: 'Aprende Fisica',
+                tema: 'fisica',
+                vistas: 28500,
+                nivel: 'Basico'
+            }
+        ],
+        'programacion':[
+            {
+                id:1,
+                titulo: 'Aprende Node',
+                lenguaje: 'javascript',
+                vistas: 15000,
+                nivel: 'Basico'
+    
+            },{
+                id:2,
+                titulo: 'Node Intermedio',
+                lenguaje: 'javascript',
+                vistas: 18000,
+                nivel: 'Intermedio'
+            },{
+                id:3,
+                titulo: 'Node Avanzado',
+                lenguaje: 'Typescript',
+                vistas: 28500,
+                nivel: 'Avanzado'
+            }
+    
+        ]
+        }
+    },
+    'tutores':{
+        'nombres':[
+            'carlo','ana','susana'
+        ],
+        
+
+    }
 }
 
 module.exports.db = infoCurso;
